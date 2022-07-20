e71da9b6e676889ac8d226dbc1179c5bb966f758
DerOracle <jambo.wivestad@gmail.com>
2022-07-20 14:04:17 +0200

    Files added

---
+++ Webpage/Login/login.sql
@@ -0,0 +1,4 @@
+-- CREATE TABLE user (username TEXT Not null PRIMARY KEY, password NOT NULL, email NOT NULL)
+
+INSERT INTO user (username, password, email)
+Values ("TordW", "mypassword", "tord.wivestad@volue.com");
